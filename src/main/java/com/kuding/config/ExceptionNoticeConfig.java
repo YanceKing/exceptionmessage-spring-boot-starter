6fb884b17b77f0bfbffcbebc381d800a095b33ef
徐晓东 <donald2007@163.com>
2018-12-28 10:35:32 +0800

    去掉多余import
    Signed-off-by: 徐晓东 <donald2007@163.com>

--- src/main/java/com/kuding/config/ExceptionNoticeConfig.java
+++ src/main/java/com/kuding/config/ExceptionNoticeConfig.java
@@ -1,7 +1,6 @@
 package com.kuding.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
