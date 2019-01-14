7d452c69d78b9ff930351d44d3e2249b8451cb33
徐晓东 <donald2007@163.com>
2019-01-14 12:38:53 +0800

    看到了一个bug，修改一下
    Signed-off-by: 徐晓东 <donald2007@163.com>

--- src/main/java/com/kuding/aop/ExceptionNoticeAop.java
+++ src/main/java/com/kuding/aop/ExceptionNoticeAop.java
@@ -21,12 +21,12 @@ public class ExceptionNoticeAop {
 		this.exceptionHandler = exceptionHandler;
 	}
 
-	@AfterThrowing(value = "@within(com.kuding.anno.ExceptionLinstener)", throwing = "e")
+	@AfterThrowing(value = "@within(com.kuding.anno.ExceptionListener)", throwing = "e")
 	public void exceptionNotice(JoinPoint joinPoint, RuntimeException e) {
 		handleException(e, joinPoint.getSignature().getName(), joinPoint.getArgs());
 	}
 
-	@AfterThrowing(value = "@annotation(com.kuding.anno.ExceptionLinstener)", throwing = "e")
+	@AfterThrowing(value = "@annotation(com.kuding.anno.ExceptionListener)", throwing = "e")
 	public void exceptionNoticeWithMethod(JoinPoint joinPoint, RuntimeException e) {
 		handleException(e, joinPoint.getSignature().getName(), joinPoint.getArgs());
 	}
